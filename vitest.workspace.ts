282a8dcf9d746b4ae1f32045972039180769585a
Daniel Polistchuck <danielp@cglab.com>
2026-02-21 14:20:04 -0300

    close(task): Verify Me [a650429b-54df-4e48-9882-7701bf38d87d]

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,10 +3,8 @@ import { defineWorkspace } from 'vitest/config';
 export default defineWorkspace([
   {
     test: {
-      name: 'agenfk',
       include: ['packages/*/src/test/**/*.{test,spec}.{ts,tsx}'],
-      exclude: ['**/dist/**', '**/node_modules/**'],
-      environment: 'jsdom', // Some UI tests might need this
+      environment: 'jsdom',
     }
   }
 ]);
