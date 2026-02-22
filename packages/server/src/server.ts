804cce89a3d32943fc54a423631a47c1dc94cc5a
Daniel Polistchuck <danielp@cglab.com>
2026-02-22 11:53:06 -0300

    close(story): Story: Migrate Utility Scripts [37894dcb-bc40-428d-b654-5a494cf311a2]

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -40,7 +40,8 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 const storage = new JSONStorageProvider();
 let dbPath: string = "";
